f4d7920edd169a5a526a1363afff20490fdd2e29
Botirov Raxmatulloh <botirovraxmatulloh@gmail.com>
2026-07-16 23:43:14 +0500

    Update project

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore' 
+import { initializeFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,5 +15,4 @@ const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const googleProvider = new GoogleAuthProvider()
-export const db = getFirestore(app) 
-
+export const db = initializeFirestore(app, { ignoreUndefinedProperties: true })
